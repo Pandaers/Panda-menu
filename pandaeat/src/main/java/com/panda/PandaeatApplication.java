acfa1189f7a6079af77018271ba62fe8c16f9d9f
korme <454334698@qq.com>
2018-08-22 17:01:40 +0800

    新开后端工程

---
+++ pandaeat/src/main/java/com/panda/PandaeatApplication.java
@@ -0,0 +1,12 @@
+package com.panda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PandaeatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PandaeatApplication.class, args);
+    }
+}
---
+++ pandaeat/src/main/java/com/panda/common/dd.java
@@ -0,0 +1,4 @@
+package com.panda.common;
+
+public class dd {
+}
