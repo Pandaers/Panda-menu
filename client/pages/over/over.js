36bef7fc4230fb52f8971f5051a3a14e2971a6cf
BaDean <294597789@qq.com>
2018-08-26 19:02:47 +0800

    后台生成二维码接口

--- client/pages/over/over.js
+++ client/pages/over/over.js
@@ -23,7 +23,20 @@ Page({
   onLoad: function (options) {
     // 页面初始化 options为页面跳转所带来的参数
     this.loadData()
-
+    wx.request({
+      url: 'https://api.weixin.qq.com/cgi-bin/wxaapp/createwxaqrcode?access_token=13_Sscd8j8CpBYN3RkXI8KSSHqTA3HDKNLCyr52zq5YYAeQlXO8fYZtdACIMTe8_m457BufsBHkpvb1B76XE8vNIrAEVSrG-8tANjJ6SLxxQB4eIJx3iJqSuBDLz0RbWx2Punc36or1h9zpel1QXNUjAHAAQX',
+      data: { 'path': "/pages/SHOP/SHOP?seatid=1", 'width': 430 },
+      method: "POST",
+      header: {
+        'content-type': 'application/json'
+      },
+      success: function (res) {
+        console.log(res.data)
+      },
+      fail: function (res) {
+        console.log('isFail')
+      }
+    })
   },
   onAddFoods: function (options) {
     // 页面初始化 options为页面跳转所带来的参数
